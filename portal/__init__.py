75f8ce38600575b3ad3d130e90d98eb066e2d66e
Jaro <jeremyvan614@gmail.com>
2019-12-12 15:28:48 -0600

    fix config logic in init

--- portal/__init__.py
+++ portal/__init__.py
@@ -6,16 +6,23 @@ from flask_misaka import markdown
 from flask_misaka import Misaka
 import logging.handlers
 import logging
+import sys
 
 __author__ = 'Jeremy Van <jeremyvan@uchicago.edu>'
 
 app = Flask(__name__)
-try:
-    # Try to read config location from .ini file
-    config_file = sys.argv[1]
-    app.config.from_pyfile(config_file)
-except:
+
+if len(sys.argv) > 1:
+    try:
+        # Try to read config location from .ini file
+        config_file = sys.argv[1]
+        app.config.from_pyfile(config_file)
+    except:
+        print("Could not read config location from {}".format(sys.argv[1]))
+else:
+    print("Reading config file from local directory")
     app.config.from_pyfile('portal.conf')
+
 app.url_map.strict_slashes = False
 
 # set up Markdown Rendering
