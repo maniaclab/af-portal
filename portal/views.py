f8ddb83c2e7ac7f83b05a48d357062be1694f818
Jaro <jeremyvan614@gmail.com>
2019-12-17 13:22:50 -0600

    logout redirect name to connect portal

--- portal/views.py
+++ portal/views.py
@@ -1058,7 +1058,7 @@ def logout():
     ga_logout_url.append(app.config['GLOBUS_AUTH_LOGOUT_URI'])
     ga_logout_url.append('?client={}'.format(app.config['PORTAL_CLIENT_ID']))
     ga_logout_url.append('&redirect_uri={}'.format(redirect_uri))
-    ga_logout_url.append('&redirect_name='+session['url_host']['display_name'])
+    ga_logout_url.append('&redirect_name=Connect Portal')
 
     # Redirect the user to the Globus Auth logout page
     return redirect(''.join(ga_logout_url))
