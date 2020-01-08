ec503b02a828f5e08eb508a46141ce7548c7aad9
Jaro <jeremyvan614@gmail.com>
2020-01-08 10:52:47 -0600

    fix aup template case sensitive

--- portal/views.py
+++ portal/views.py
@@ -1108,7 +1108,7 @@ def aup():
     domain_name = request.headers['Host']
     with open(brand_dir+'/'+domain_name+'/signup_content/signup_modal.md', "r") as file:
         aup_md = file.read()
-    return render_template('aup.html', aup_md=aup_md)
+    return render_template('AUP.html', aup_md=aup_md)
 
 
 @app.route('/login', methods=['GET'])
