954f5fb51d4999d326728e77f21bf64866126cf0
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 15:49:14 -0500

    try to import one function from slate api

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -2,6 +2,4 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 import requests
 from portal import app
 from portal.decorators import authenticated
-
-slate_api_token = app.config['SLATE_API_TOKEN']
-slate_api_endpoint = app.config['SLATE_API_ENDPOINT']
+from portal.slate_api import get_app_config
