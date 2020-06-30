ca3317b1466fbd1fa7c20ba0fead1ac048f774e9
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 16:06:37 -0500

    test adding json package

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -5,5 +5,5 @@ from portal.decorators import authenticated
 from portal.slate_api import get_app_config, get_app_readme, list_users_instances_request, get_instance_details, get_instance_logs, delete_instance
 from portal.connect_api import get_user_profile
 import os
-# import yaml, json
+import json
 from base64 import b64encode
