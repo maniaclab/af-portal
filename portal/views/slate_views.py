d252a7a3b34a6ff7c5288e99c6d96b724395ae9c
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 15:36:25 -0500

    try to locate slate config info

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -2,3 +2,6 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 import requests
 from portal import app
 from portal.decorators import authenticated
+
+slate_api_token = app.config['SLATE_API_TOKEN']
+slate_api_endpoint = app.config['SLATE_API_ENDPOINT']
