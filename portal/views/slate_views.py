c07359631b5f16b1e71b02a8a6c4b403a4d96320
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 16:01:42 -0500

    add all necessary imports from slate api into slate views

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -2,4 +2,5 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 import requests
 from portal import app
 from portal.decorators import authenticated
-from portal.slate_api import generateToken
+from portal.slate_api import get_app_config, get_app_readme, list_users_instances_request, get_instance_details, get_instance_logs, delete_instance
+from portal.connect_api import get_user_profile
