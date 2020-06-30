5665c6ea439c34f269ffff686850b505816221e2
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 16:05:34 -0500

    test import b64encode package

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -6,4 +6,4 @@ from portal.slate_api import get_app_config, get_app_readme, list_users_instance
 from portal.connect_api import get_user_profile
 import os
 # import yaml, json
-# from base64 import b64encode
+from base64 import b64encode
