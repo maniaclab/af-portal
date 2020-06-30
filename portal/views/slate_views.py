ad4353f35ee4f331a3d4911a2a546a6474349ad7
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 15:26:33 -0500

    add imports in slate views file

---
+++ portal/views/slate_views.py
@@ -0,0 +1,9 @@
+from flask import session, request, render_template, jsonify, redirect, url_for, flash
+import requests
+from portal import app
+from portal.decorators import authenticated
+from portal.slate_api import get_app_config, get_app_readme, list_users_instances_request, get_instance_details, get_instance_logs, delete_instance
+from portal.connect_api import get_user_profile
+import os
+import yaml, json
+from base64 import b64encode
