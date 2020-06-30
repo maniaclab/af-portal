3581f863543fe76c9c66ef4e6d24d65742454bc3
Jeremy Van <jeremyvan614@gmail.com>
2020-06-30 15:29:45 -0500

    rm connect api import

--- portal/views/slate_views.py
+++ portal/views/slate_views.py
@@ -3,4 +3,3 @@ import requests
 from portal import app
 from portal.decorators import authenticated
 from portal.slate_api import get_app_config, get_app_readme, list_users_instances_request, get_instance_details, get_instance_logs, delete_instance
-from portal.connect_api import get_user_profile
