abdb3e64bdbc6cb5ab190c8b29c23dc0a902f016
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2021-06-14 15:03:50 -0500

    remove unused imports

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
-import json
 
 # from flask import Markup
-from flask_misaka import markdown
 from flask_misaka import Misaka
 import logging.handlers
-import logging
 import sys
 
 __author__ = "MANIAC Lab <gardnergroup@lists.uchicago.edu>"
