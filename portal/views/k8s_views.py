868835837c86da301d9332f060ba8e328d464f39
Andrew <rolyata47@gmail.com>
2021-12-15 13:14:49 -0600

    Adding import for k8s_api

--- portal/views/k8s_views.py
+++ portal/views/k8s_views.py
@@ -2,6 +2,7 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 import requests
 from portal import app
 from portal.decorators import authenticated
+import k8s_api
 
 @app.route("/instances/deploy_jupyter", methods=["GET", "POST"])
 @authenticated
