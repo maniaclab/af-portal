ab74b2ddd26af6a5dd1a4f27ace65f68d43c724c
Andrew <rolyata47@gmail.com>
2021-12-15 12:47:53 -0600

    Adding imports

--- portal/views/k8s_views.py
+++ portal/views/k8s_views.py
@@ -1,3 +1,8 @@
+from flask import session, request, render_template, jsonify, redirect, url_for, flash
+import requests
+from portal import app
+from portal.decorators import authenticated
+
 @app.route("/instances/deploy_jupyter", methods=["GET", "POST"])
 @authenticated
 def deploy_jupyter():
