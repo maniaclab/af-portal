f4dd41176b2db161c8b863b3694b5569f0cb871b
Andrew <rolyata47@gmail.com>
2021-12-15 13:43:10 -0600

    Updating k8s api

--- portal/views/k8s_views.py
+++ portal/views/k8s_views.py
@@ -4,9 +4,35 @@ from portal import app
 from portal.decorators import authenticated
 import k8s_api
 
-@app.route("/instances/deploy_jupyter", methods=["GET", "POST"])
+@app.route("/jupyter/deploy", methods=["GET", "POST"])
 @authenticated
-def deploy_jupyter():
+def deploy_jupyter_notebook():
     k8s_api.create_jupyter_notebook()
 
-    return redirect(url_for("view_instances"))
+    return redirect(url_for("view_jupyter"))
+
+@app.route("/jupyter/view", methods=["GET", "POST"])
+@authenticated
+def view_jupyter_notebooks():
+    """Connect groups"""
+    query = {"token": slate_api_token, "dev": "true"}
+    if request.method == "GET":
+        app_name = "jupyter-notebook"
+        profile = get_user_profile(session["unix_name"])
+        try:
+            public_key = profile["metadata"]["public_key"]
+        except:
+            public_key = None
+        instances = list_users_instances_request(session)
+
+        # Check user's member status of connect group specifically
+        connect_group = session["url_host"]["unix_name"]
+        user_status = get_user_connect_status(session["unix_name"], connect_group)
+
+        return render_template(
+            "instances.html",
+            name=app_name,
+            public_key=public_key,
+            instances=instances,
+            user_status=user_status,
+        )
