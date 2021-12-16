b5fc9234dae4d78f69d410aee5cdf0fff1950772
Andrew <rolyata47@gmail.com>
2021-12-16 17:31:40 -0600

    bugfix

--- portal/views/k8s_views.py
+++ portal/views/k8s_views.py
@@ -2,12 +2,12 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 import requests
 from portal import app
 from portal.decorators import authenticated
-import k8s_api
+from portal.k8s_api import create_jupyter_notebook
 
 @app.route("/jupyter/deploy", methods=["GET", "POST"])
 @authenticated
 def deploy_jupyter_notebook():
-    k8s_api.create_jupyter_notebook()
+    create_jupyter_notebook()
 
     return redirect(url_for("view_instances"))
 
