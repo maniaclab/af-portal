e07c3bcf3c3fa6e5807b90b0aef5157c6645475b
Andrew <rolyata47@gmail.com>
2021-12-21 12:49:18 -0600

    adding import for slate_views module and view_instances function

--- portal/views/k8s_views.py
+++ portal/views/k8s_views.py
@@ -3,7 +3,7 @@ import requests
 from portal import app
 from portal.decorators import authenticated
 from portal.k8s_api import create_jupyter_notebook
-from portal.views.slate_views import view_instances
+from slate_views import view_instances
 
 @app.route("/jupyter/deploy", methods=["GET", "POST"])
 @authenticated
