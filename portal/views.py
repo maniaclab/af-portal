2e0c0f8cb640e404fff16b1c75f1178011735845
Andrew Taylor <rolyata47@gmail.com>
2022-02-04 16:59:51 -0600

    Updated portals; updated about page; logout redirects to home page

--- portal/views.py
+++ portal/views.py
@@ -733,7 +733,8 @@ def logout():
     ga_logout_url.append("&redirect_name=Connect Portal")
 
     # Redirect the user to the Globus Auth logout page
-    return redirect("".join(ga_logout_url))
+    # return redirect("".join(ga_logout_url))
+    return redirect(url_for("home"))
 
 
 @app.route("/profile/new", methods=["GET", "POST"])
