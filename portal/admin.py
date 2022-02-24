454fbc6fa9d578e4a4c5a7c35dea73c20f2ab28a
Andrew Taylor <rolyata47@gmail.com>
2022-02-23 18:36:51 -0600

    Adding admin authorization for admin pages

--- portal/admin.py
+++ portal/admin.py
@@ -1,4 +1,5 @@
 import requests
+from flask import session
 from dateutil.parser import parse
 from datetime import datetime 
 from datetime import timedelta
@@ -9,72 +10,82 @@ ciconnect_api_token = app.config["CONNECT_API_TOKEN"]
 ciconnect_api_endpoint = app.config["CONNECT_API_ENDPOINT"]
 mailgun_api_token = app.config["MAILGUN_API_TOKEN"]
 
+def authorized():
+    return session.get('admin') == 'admin'
+
 def get_users(group):
-    url = ciconnect_api_endpoint + "/v1alpha1/groups/" + group + "/members?token=" + ciconnect_api_token
-    users = requests.get(url).json()
-    return users
+    if authorized():
+        url = ciconnect_api_endpoint + "/v1alpha1/groups/" + group + "/members?token=" + ciconnect_api_token
+        users = requests.get(url).json()
+        return users
 
 def get_user_profile(username):
-    url = ciconnect_api_endpoint + "/v1alpha1/users/" + username + "?token=" + ciconnect_api_token
-    user_profile = requests.get(url).json()
-    return user_profile
+    if authorized():
+        url = ciconnect_api_endpoint + "/v1alpha1/users/" + username + "?token=" + ciconnect_api_token
+        user_profile = requests.get(url).json()
+        return user_profile
 
 def get_user_profiles(group):
-    user_list = []
-    users = get_users(group)
+    if authorized():
+        user_list = []
+        users = get_users(group)
 
-    for member in users['memberships']:
-        username = member['user_name']
-        user_profile = get_user_profile(username)
-        username = user_profile['metadata']['unix_name'] 
-        join_date = parse(user_profile['metadata']['join_date']).strftime('%Y-%m-%d')
-        email = user_profile['metadata']['email']
-        institution = user_profile['metadata']['institution']
-        name = user_profile['metadata']['name']
-        phone = user_profile['metadata']['phone']
-        user_entry = {
-            'username': username, 
-            'email': email, 
-            'institution': institution,
-            'join_date': join_date,
-        }
-        user_list.append(user_entry)
-
-    return user_list
+        for member in users['memberships']:
+            username = member['user_name']
+            user_profile = get_user_profile(username)
+            username = user_profile['metadata']['unix_name'] 
+            join_date = parse(user_profile['metadata']['join_date']).strftime('%Y-%m-%d')
+            email = user_profile['metadata']['email']
+            institution = user_profile['metadata']['institution']
+            name = user_profile['metadata']['name']
+            phone = user_profile['metadata']['phone']
+            user_entry = {
+                'username': username, 
+                'email': email, 
+                'institution': institution,
+                'join_date': join_date,
+            }
+            user_list.append(user_entry)
+        return user_list
 
 def get_email_list(group):
-    email_list = []
-    report = get_user_profiles(group)
-    for user in report:
-        email_list.append(user['email'])
-    return email_list
+    if authorized():
+        email_list = []
+        report = get_user_profiles(group)
+        for user in report:
+            email_list.append(user['email'])
+        return email_list
 
 def update_user_institution(username, institution):
-    query = {"token": ciconnect_api_token}
-    json_data = {'apiVersion': 'v1alpha1', 'kind': 'User', 'metadata': {'institution': institution}}
-    url = ciconnect_api_endpoint + "/v1alpha1/users/" + username
-    try:
-        requests.put(url, params=query, json=json_data)
-        logger.info("Updated user %s. Set institution to %s." %(username, institution))
-        return True
-    except Exception as err:
-        logger.error("Error updating institution for user %s." %username)
-        return False
+    if authorized():
+        query = {"token": ciconnect_api_token}
+        json_data = {'apiVersion': 'v1alpha1', 'kind': 'User', 'metadata': {'institution': institution}}
+        url = ciconnect_api_endpoint + "/v1alpha1/users/" + username
+        try:
+            requests.put(url, params=query, json=json_data)
+            logger.info("Updated user %s. Set institution to %s." %(username, institution))
+            return True
+        except Exception as err:
+            logger.error("Error updating institution for user %s." %username)
+            return False
     
 def email_users(sender, recipients, subject, body):
-    try: 
-        resp = requests.post("https://api.mailgun.net/v3/api.ci-connect.net/messages",
-            auth=("api", mailgun_api_token),
-            data={
-                "from": "<" + sender + ">",
-                "to": [sender],
-                "bcc": recipients,
-                "subject": subject,
-                "html": body,
-            }
-        )
-        return resp
-    except:
-        logger.error("Error sending email to all users")
-        return None
+    if authorized():
+        try: 
+            logger.info("Sending email...")
+            resp = requests.post("https://api.mailgun.net/v3/api.ci-connect.net/messages",
+                auth=("api", mailgun_api_token),
+                data={
+                    "from": "<" + sender + ">",
+                    "to": [sender],
+                    "bcc": recipients,
+                    "subject": subject,
+                    "text": body
+                }
+            )
+            logger.info("Response status: " + resp.status_code)
+            logger.info("Response text: " + resp.text)
+            return resp
+        except:
+            logger.error("Error sending email to all users")
 
--- portal/views/admin_views.py
+++ portal/views/admin_views.py
@@ -2,51 +2,57 @@ from flask import session, request, render_template, jsonify, redirect, url_for,
 from portal import logger
 from portal import app
 from portal import admin
+from portal.admin import authorized
 from portal.decorators import authenticated
 from portal.connect_api import get_user_profile, get_user_connect_status
 
 @app.route("/admin/email", methods=["GET"])
 @authenticated
 def admin_email():
-    return render_template("admin_email.html")
+    if authorized():
+        return render_template("admin_email.html")
+    return render_template("404.html")
 
 @app.route("/admin/users", methods=["GET"])
 @authenticated
 def admin_users():
-    return render_template("admin_users.html")
+    if authorized(): 
+        return render_template("admin_users.html")
+    return render_template("404.html")
 
 @app.route("/admin/user_profiles", methods=["GET"])
 @authenticated
 def admin_get_user_profiles():
-    try:
+    if authorized():
         user_profiles = admin.get_user_profiles('root.atlas-af')
         return jsonify(user_profiles)
-    except:
-        return None
 
 @app.route("/admin/update_user_institution", methods=["POST"])
 @authenticated
 def admin_update_user_institution():
-    username = request.form['username']
-    institution = request.form['institution']
-    status = admin.update_user_institution(username, institution)
-    return jsonify(success=status)
+    if authorized():
+        username = request.form['username']
+        institution = request.form['institution']
+        status = admin.update_user_institution(username, institution)
+        return jsonify(success=status)
 
 @app.route("/admin/email_users", methods=["POST"])
 @authenticated
 def admin_email_users():
-    sender = 'noreply@ci-connect.net'
-    # recipients = admin.get_email_list('root.atlas-af')
-    recipients = ["rolyata@uchicago.edu"]
-    subject = request.form['subject']
-    body = request.form['body']
-    print("Sender: " + sender)
-    print("Recipients: " + str(recipients))
-    print("Subject: " + subject)
-    print("Body: " + body)
-    resp = admin.email_users(sender, recipients, subject, body)
-    if resp and resp.status_code == 200:
-        flash('Sent email successfully', 'success')
-    else:
-        flash('Error sending email', 'warning')
-    return redirect(url_for('admin_email'))
+    if authorized():
+        sender = 'noreply@ci-connect.net'
+        # recipients = admin.get_email_list('root.atlas-af')
+        recipients = [""]
+        subject = request.form['subject']
+        body = request.form['body']
+        print("Sender: " + sender)
+        print("Recipients: " + str(recipients))
+        print("Subject: " + subject)
+        print("Body: " + body)
+        resp = admin.email_users(sender, recipients, subject, body)
+        if resp and resp.status_code == 200:
+            flash('Sent email successfully', 'success')
+        else:
+            flash('Error sending email', 'warning')
+        return redirect(url_for('admin_email'))
+    return render_template("404.html")
