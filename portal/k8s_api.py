d7f3f585c3d49598f4fea6b7d55b8bafdf4a1b88
Andrew Taylor <rolyata47@gmail.com>
2022-04-18 15:27:41 -0500

    Updating JupyterLab page to have one Status column that shows current status

--- portal/k8s_api.py
+++ portal/k8s_api.py
@@ -491,7 +491,6 @@ def remove_notebook(notebook_id):
     core_v1_api.delete_namespaced_service(notebook_id, namespace)
     networking_v1_api = client.NetworkingV1Api()
     networking_v1_api.delete_namespaced_ingress(notebook_id, namespace)
-    core_v1_api.delete_namespaced_secret(notebook_id, namespace)
     logger.info("Removing notebook %s in namespace %s" %(notebook_id, namespace))
 
 def remove_user_notebook(notebook_name, username):
@@ -504,7 +503,6 @@ def remove_user_notebook(notebook_name, username):
             core_v1_api.delete_namespaced_pod(notebook_id, namespace)
             core_v1_api.delete_namespaced_service(notebook_id, namespace)
             networking_v1_api.delete_namespaced_ingress(notebook_id, namespace)
-            core_v1_api.delete_namespaced_secret(notebook_id, namespace)
             logger.info('Removing notebook %s' %notebook_id)
         else:
             logger.warning('Notebook %s does not belong to user %s' %(notebook_id, username))
