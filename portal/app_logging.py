1bca988fe74e6cd566d90ab540eeaa4f8f2ad9ab
Andrew Taylor <rolyata47@gmail.com>
2022-05-23 18:52:52 -0500

    The init_logging method is being called multiple times Added a condition so that the logger is initialized only once

--- portal/app_logging.py
+++ portal/app_logging.py
@@ -2,17 +2,18 @@ import logging
 
 def init_logger():
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    fh = logging.FileHandler('ciconnect-portal.log')
-    fh.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s;%(module)s;%(funcName)s;%(levelname)s;%(message)s")
-    ch.setFormatter(formatter)
-    fh.setFormatter(formatter)
-    logger.propagate = False
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+    if not logger.hasHandlers():
+        logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        fh = logging.FileHandler('af-portal.log')
+        fh.setLevel(logging.INFO)
+        formatter = logging.Formatter("%(asctime)s;%(module)s;%(funcName)s;%(levelname)s;%(message)s")
+        ch.setFormatter(formatter)
+        fh.setFormatter(formatter)
+        logger.propagate = False
+        logger.addHandler(ch)
+        logger.addHandler(fh)
     return logger
 
 def get_logger():
