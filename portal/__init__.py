caa6e2551665890d17801d091d48b82e1afeac53
Andrew Taylor <rolyata47@gmail.com>
2022-08-13 21:07:50 -0500

    Adding login

--- portal/__init__.py
+++ portal/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from jinja2_markdown import MarkdownExtension
 
 app = Flask(__name__)
+app.config.from_pyfile("secrets/portal.conf")
 app.jinja_env.add_extension(MarkdownExtension)
 
 import portal.views
--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,7 @@
 from portal import app
-from flask import render_template
+from flask import session, request, render_template, url_for, redirect
+import globus_sdk
+from urllib.parse import urlparse, urljoin
 
 @app.route("/", methods=["GET"])
 def home():
@@ -12,3 +14,44 @@ def about():
 @app.route("/signup", methods=["GET"])
 def signup():
     return render_template("signup.html")
+
+@app.route("/login", methods=["GET"])
+def login():
+    redirect_uri = url_for("login", _external=True)
+    client = globus_sdk.ConfidentialAppAuthClient(app.config["CLIENT_ID"], app.config["CLIENT_SECRET"])
+    client.oauth2_start_flow(redirect_uri, refresh_tokens=True)
+    if "code" not in request.args:
+        next_url = get_safe_redirect()
+        params = {"next": next_url}
+        auth_uri = client.oauth2_get_authorize_url(additional_params=params)
+        return redirect(auth_uri)
+    else:
+        code = request.args.get("code")
+        tokens = client.oauth2_exchange_code_for_tokens(code)
+        session.update(tokens=tokens.by_resource_server, is_authenticated=True)
+        return redirect(url_for("home"))
+
+@app.route("/logout")
+def logout():
+    client = globus_sdk.ConfidentialAppAuthClient(app.config["CLIENT_ID"], app.config["CLIENT_SECRET"])
+    for token in (token_info["access_token"] for token_info in session["tokens"].values()):
+        client.oauth2_revoke_token(token)
+    session.clear()
+    redirect_uri = url_for("home", _external=True)
+    globus_logout_url = ("https://auth.globus.org/v2/web/logout?client=%s&redirect_uri=%s&redirect_name=Simple Portal" %(app.config["CLIENT_ID"], redirect_uri))
+    return redirect(globus_logout_url)
+
+def is_safe_redirect_url(target):
+  host_url = urlparse(request.host_url)
+  redirect_url = urlparse(urljoin(request.host_url, target))
+  return redirect_url.scheme in ('http', 'https') and \
+    host_url.netloc == redirect_url.netloc
+
+def get_safe_redirect():
+  url =  request.args.get('next')
+  if url and is_safe_redirect_url(url):
+    return url
+  url = request.referrer
+  if url and is_safe_redirect_url(url):
+    return url
+  return '/'
