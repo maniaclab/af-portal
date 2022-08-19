3f020396f4e6677adf6a32de3fdf5d0fb9f8f920
Andrew Taylor <rolyata47@gmail.com>
2022-08-19 10:27:14 -0500

    Update

--- ssl/gen.sh
+++
@@ -1,7 +0,0 @@
-openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
-
-# Support for Microsoft Edge
-# 
-# Steps:
-# 1. Go to edge://flags and search for localhost
-# 2. Enable the flag "Allow invalid certificates for resources loaded from localhost"
