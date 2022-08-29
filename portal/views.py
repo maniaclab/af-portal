303f99513d45f500c23018e42e39b32c0c48c1f9
Andrew Taylor <rolyata47@gmail.com>
2022-08-29 18:30:55 -0500

    Updating code

--- portal/views.py
+++ portal/views.py
@@ -346,17 +346,17 @@ def add_group_member(unix_name, group_name):
         flash("Error adding %s to group %s" %(unix_name, group_name), "warning")
         return redirect(url_for("groups", group_name=group_name))
 
-@app.route("/groups/<group_name>/delete_group_member/<unix_name>")
+@app.route("/groups/<group_name>/remove_group_member/<unix_name>")
 @auth.admins_only
 def remove_group_member(unix_name, group_name):
     try:
         success = connect.remove_user_from_group(unix_name, group_name)
         flash("Removed %s from group %s" %(unix_name, group_name), "success")
-        return redirect(url_for("groups", group_name=group_name))
+        return jsonify(success=True)
     except Exception as err:
         logger.error(str(err))
         flash("Error removing %s from group %s" %(unix_name, group_name), "warning")
-        return redirect(url_for("groups", group_name=group_name))
+        return jsonify(success=False)
 
 @app.route("/groups/<group_name>/approve_subgroup_request/<subgroup_name>")
 @auth.admins_only
