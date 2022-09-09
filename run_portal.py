b00f857a12276780b5a5fe9e56793d40404c907c
Andrew Taylor <rolyata47@gmail.com>
2022-09-09 14:30:59 -0500

    Updating run_portal.py

--- run_portal.py
+++ run_portal.py
@@ -1,4 +1,4 @@
 from portal import app
 
 if __name__ == "__main__":
-    app.run(host="localhost", ssl_context=('./ssl/cert.pem', './ssl/key.pem'))
+    app.run(host="localhost", ssl_context=("./ssl/cert.pem", "./ssl/key.pem"))
