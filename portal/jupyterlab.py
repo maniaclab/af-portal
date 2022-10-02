9e26659b4050f105833f74932b8b68613fac5643
Andrew Taylor <rolyata47@gmail.com>
2022-10-01 22:33:21 -0500

    Adding docstrings to jupyterlab.py

--- portal/jupyterlab.py
+++ portal/jupyterlab.py
@@ -1,72 +1,84 @@
-# This module supports the JupyterLab service
-# 
-# Functionality:
-# =============== 
-#
-# 1. The load_kube_config method loads a kubeconfig file (either the file specified or ~/.kube/config)
-# 2. The start_notebook_maintenance method starts a thread for removing expired notebooks
-# 3. The deploy_notebook method lets a user deploy a notebook onto our Kubernetes cluster
-# 4. The get_notebook function lets a user get data for a single notebook
-# 5. The get_notebooks function lets a user get data for all of a user's notebooks
-# 6. The remove_notebook function lets a user remove a notebook
-# 7. The list_notebook function returns a list of the names of all currently running notebooks
-# 8. The get_gpus function returns GPU info for all of our GPU products 
-#
-# Dependencies:
-# =============== 
-# 
-# 1. A portal.conf configuration file (af-portal/portal/secrets/portal.conf)
-# 2. A kubeconfig file (either a file specified in portal.conf, or a file at the default location, ~/.kube/config)
-#
-# Example usage:
-# =============== 
-#
-# Example #1:
-#
-# cd <path>/<to>/af-portal
-# python 
-# >>> from portal import jupyterlab
-# >>> from pprint import pprint
-# >>> jupyterlab.load_kube_config()
-# >>> notebook = jupyterlab.get_notebook('example-notebook-1')
-# >>> pprint(notebook)
-#
-# Example #2:
-#
-# cd <path>/<to>/af-portal
-# python 
-# >>> from portal import jupyterlab
-# >>> from pprint import pprint
-# >>> jupyterlab.load_kube_config()
-# >>> notebooks = jupyterlab.get_notebooks('my-username')
-# >>> pprint(notebooks)
-#
-# Example #3:
-#
-# cd <path>/<to>/af-portal
-# python 
-# >>> from portal import jupyterlab
-# >>> from pprint import pprint
-# >>> jupyterlab.load_kube_config()
-# >>> settings = {
-# 'notebook_name': 'MyNotebook',
-# 'notebook_id': 'mynotebook',
-# 'image': 'ml-platform:latest',
-# 'owner': 'myusername',
-# 'owner_uid': 2468,
-# 'globus_id': 'alphanumeric string with hyphens',
-# 'cpu_request': 1,
-# 'cpu_limit': 2,
-# 'memory_request': '1Gi',
-# 'memory_limit': '2Gi',
-# 'gpu_request': 1,
-# 'gpu_limit': 1,
-# 'gpu_memory': 4864,
-# 'hours_remaining': 168
-# }
-# >>> jupyterlab.deploy_notebook(**settings)
-# >>> notebook = jupyterlab.get_notebook('mynotebook', url=True)
-# >>> pprint(notebook)
+'''
+This module supports the JupyterLab service
+
+Functionality:
+=============== 
+
+1. The load_kube_config method loads a kubeconfig file (either the file specified or ~/.kube/config)
+2. The start_notebook_maintenance method starts a thread for removing expired notebooks
+3. The deploy_notebook method lets a user deploy a notebook onto our Kubernetes cluster
+4. The get_notebook function lets a user get data for a single notebook
+5. The get_notebooks function lets a user get data for all of a user's notebooks
+6. The remove_notebook function lets a user remove a notebook
+7. The list_notebook function returns a list of the names of all currently running notebooks
+8. The get_gpus function returns GPU info for all of our GPU products 
+
+Dependencies:
+=============== 
+
+1. A portal.conf configuration file (af-portal/portal/secrets/portal.conf)
+2. A kubeconfig file (either a file specified in portal.conf, or a file at the default location, ~/.kube/config)
+
+Example usage:
+=============== 
+
+Example #1:
+
+cd <path>/<to>/af-portal
+python 
+>>> from portal import jupyterlab
+>>> from pprint import pprint
+>>> jupyterlab.load_kube_config()
+>>> notebook = jupyterlab.get_notebook('example-notebook-1')
+>>> pprint(notebook)
+
+Example #2:
+
+cd <path>/<to>/af-portal
+python 
+>>> from portal import jupyterlab
+>>> from pprint import pprint
+>>> jupyterlab.load_kube_config()
+>>> notebooks = jupyterlab.get_notebooks('my-username')
+>>> pprint(notebooks)
+
+Example #3:
+
+cd <path>/<to>/af-portal
+python 
+>>> from portal import jupyterlab
+>>> from pprint import pprint
+>>> jupyterlab.load_kube_config()
+>>> settings = {
+'notebook_name': 'MyNotebook',
+'notebook_id': 'mynotebook',
+'image': 'ml-platform:latest',
+'owner': 'myusername',
+'owner_uid': 2468,
+'globus_id': 'alphanumeric string with hyphens',
+'cpu_request': 1,
+'cpu_limit': 2,
+'memory_request': '1Gi',
+'memory_limit': '2Gi',
+'gpu_request': 1,
+'gpu_limit': 1,
+'gpu_memory': 4864,
+'hours_remaining': 168
+}
+>>> jupyterlab.deploy_notebook(**settings)
+>>> notebook = jupyterlab.get_notebook('mynotebook', url=True)
+>>> pprint(notebook)
+
+Example #4
+
+cd <path>/<to>/af-portal
+python 
+>>> from portal import jupyterlab
+>>> help(jupyterlab)
+>>> help(jupyterlab.deploy_notebook)
+>>> help(jupyterlab.get_notebook)
+>>> help(jupyterlab.get_notebooks)
+'''
 import yaml
 import time
 import datetime
@@ -107,26 +119,28 @@ def start_notebook_maintenance():
     threading.Thread(target=clean).start()
     logger.info('Started notebook maintenance')
 
-# Deploys a Jupyter notebook on our Kubernetes cluster.
-#
-# Function parameters:
-# (All parameters below are required.)
-#
-# notebook_name: (string) The display name of the notebook
-# notebook_id: (string) A unique name for the notebook
-# image: (string) The Docker image that will be run in the pod's container
-# owner: (string) The username of the notebook's owner 
-# owner_uid: (integer) The numeric ID of the notebook's owner 
-# globus_id: (string) The Globus ID of the notebook's owner
-# cpu_request: (integer) The number of CPU cores to request from the k8s cluster
-# cpu_limit: (integer) The max number of CPU cores that can be allocated to this pod
-# memory_request: (string) The amount of memory to request from the k8s cluster (e.g. '1Gi')
-# memory_limit: (string) The max amount of memory that can be allocated to this pod
-# gpu_request: (integer) The number of GPU instances to request from the k8s cluster
-# gpu_limit: (integer) The max number of GPU instances that can be allocated to this pod
-# gpu_memory: (integer) Selects a GPU product based on its memory, in megabytes (e.g. 4864)
-# hours_remaining: (integer) The duration of the notebook in hours
 def deploy_notebook(**settings):
+    '''
+    Deploys a Jupyter notebook on our Kubernetes cluster.
+
+    Function parameters:
+    (All parameters below are required.)
+
+    notebook_name: (string) The display name of the notebook
+    notebook_id: (string) A unique name for the notebook
+    image: (string) The Docker image that will be run in the pod's container
+    owner: (string) The username of the notebook's owner 
+    owner_uid: (integer) The numeric ID of the notebook's owner 
+    globus_id: (string) The Globus ID of the notebook's owner
+    cpu_request: (integer) The number of CPU cores to request from the k8s cluster
+    cpu_limit: (integer) The max number of CPU cores that can be allocated to this pod
+    memory_request: (string) The amount of memory to request from the k8s cluster (e.g. '1Gi')
+    memory_limit: (string) The max amount of memory that can be allocated to this pod
+    gpu_request: (integer) The number of GPU instances to request from the k8s cluster
+    gpu_limit: (integer) The max number of GPU instances that can be allocated to this pod
+    gpu_memory: (integer) Selects a GPU product based on its memory, in megabytes (e.g. 4864)
+    hours_remaining: (integer) The duration of the notebook in hours
+    '''
     settings['namespace'] = namespace
     settings['domain_name'] = app.config['DOMAIN_NAME']
     settings['token'] = b64encode(os.urandom(32)).decode()
@@ -151,16 +165,18 @@ def deploy_notebook(**settings):
     api.create_namespaced_ingress(namespace=namespace, body=ingress)
     logger.info('Deployed notebook %s' %settings['notebook_name'])
 
-# Looks up a notebook by name or by pod. Returns a dict.
-#
-# Function parameters:
-# (Either a name or a pod is required to look up a notebook.)
-#
-# name: (string) The name of the notebook
-# pod: (object) The pod object returned by the kubernetes client
-# log: (boolean) When log is True, the pod log is included in the dict that gets returned
-# url: (boolean) When url is True, the notebook URL is included in the dict that gets returned
 def get_notebook(name=None, pod=None, log=False, url=False):
+    '''
+    Looks up a notebook by name or by pod. Returns a dict.
+
+    Function parameters:
+    (Either a name or a pod is required to look up a notebook.)
+
+    name: (string) The name of the notebook
+    pod: (object) The pod object returned by the kubernetes client
+    log: (boolean) When log is True, the pod log is included in the dict that gets returned
+    url: (boolean) When url is True, the notebook URL is included in the dict that gets returned
+    '''
     api = client.CoreV1Api()
     if pod is None:
         pod = api.read_namespaced_pod(name=name.lower(), namespace=namespace)
@@ -188,12 +204,14 @@ def get_notebook(name=None, pod=None, log=False, url=False):
         notebook['url'] = get_url(pod)
     return notebook
 
-# Retrieves a user's notebooks, or the notebooks for all users. Returns an array of dicts.
-#
-# Function parameters:
-#
-# owner: (string) The username of the owner. When owner is None, the function returns all notebooks for all users.
 def get_notebooks(owner=None):
+    '''
+    Retrieves a user's notebooks, or the notebooks for all users. Returns an array of dicts.
+
+    Function parameters:
+
+    owner: (string) The username of the owner. When owner is None, the function returns all notebooks for all users.
+    '''
     notebooks = []
     api = client.CoreV1Api()
     selector = 'k8s-app=jupyterlab' if owner is None else 'k8s-app=jupyterlab,owner=%s' %owner
@@ -207,8 +225,8 @@ def get_notebooks(owner=None):
             logger.error(str(err))
     return notebooks
 
-# Returns a list of the names of all notebooks in the namespace.
 def list_notebooks():
+    ''' Returns a list of the names of all notebooks in the namespace. '''
     notebooks = []
     api = client.CoreV1Api()
     pods = api.list_namespaced_pod(namespace=namespace, label_selector='k8s-app=jupyterlab').items
@@ -216,8 +234,8 @@ def list_notebooks():
         notebooks.append(pod.metadata.name)
     return notebooks
 
-# Removes a notebook from the namespace, and all Kubernetes objects associated with the notebook.
 def remove_notebook(notebook_name):
+    ''' Removes a notebook from the namespace, and all Kubernetes objects associated with the notebook. '''
     notebook_id = notebook_name.lower()
     core_v1_api = client.CoreV1Api()
     core_v1_api.delete_namespaced_pod(notebook_id, namespace)
@@ -227,29 +245,29 @@ def remove_notebook(notebook_name):
     networking_v1_api.delete_namespaced_ingress(notebook_id, namespace)
     logger.info('Removed notebook %s from namespace %s' %(notebook_id, namespace))
 
-# Returns a boolean indicating whether a notebook name is available for use.
 def notebook_name_available(notebook_name):
+    ''' Returns a boolean indicating whether a notebook name is available for use. '''
     notebook_id = notebook_name.lower()
     core_v1_api = client.CoreV1Api()
     pods = core_v1_api.list_namespaced_pod(namespace, label_selector='notebook-id={0}'.format(notebook_id))
     return len(pods.items) == 0
 
-# Returns a default notebook name that is available for use, e.g. testuser-notebook-3.
 def generate_notebook_name(owner):
+    ''' Returns a default notebook name that is available for use, e.g. testuser-notebook-3. '''
     for i in range(1, 20):
         notebook_name = f'{owner}-notebook-{i}'
         if notebook_name_available(notebook_name):
             return notebook_name
     return None
 
-# Returns a tuple of Docker images that are supported by the JupyterLab service.
 def supported_images():
+    ''' Returns a tuple of Docker images that are supported by the JupyterLab service. '''
     return ('hub.opensciencegrid.org/usatlas/ml-platform:latest', 'hub.opensciencegrid.org/usatlas/ml-platform:conda', 
             'hub.opensciencegrid.org/usatlas/ml-platform:julia', 'hub.opensciencegrid.org/usatlas/ml-platform:lava',
             'hub.opensciencegrid.org/usatlas/ml-platform:centos7-experimental')
 
-# Returns an array of GPU products and their availability.
 def get_gpus():
+    ''' Returns an array of GPU products and their availability. '''
     gpus = dict()
     api = client.CoreV1Api()
     nodes = api.list_node(label_selector='gpu=true')
@@ -271,8 +289,8 @@ def get_gpus():
         gpu['available'] = max(gpu['available'], 0)
     return sorted(gpus.values(), key=lambda gpu : gpu['memory'])
 
-# Looks up a GPU product by its memory size. Returns a dict containing its product info and availability.
 def get_gpu(memory):
+    ''' Looks up a GPU product by its memory size. Returns a dict containing its product info and availability. '''
     gpu = dict()
     api = client.CoreV1Api()
     nodes=api.list_node(label_selector='gpu=true,nvidia.com/gpu.memory=%s' %memory)
