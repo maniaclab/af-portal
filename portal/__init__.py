1509ae22603eb6b51623b14bc98ca4d29eb7a988
Ilija Vukotic <ivukotic@cern.ch>
2022-11-01 19:00:23 -0500

    log

--- portal/__init__.py
+++ portal/__init__.py
@@ -10,19 +10,28 @@ def create_app():
     app.jinja_env.add_extension(MarkdownExtension)
     csrf = CSRFProtect(app)
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s;%(module)s;%(funcName)s;%(levelname)s;%(message)s')
-    logger.propagate = False
+    # logger = logging.getLogger()
+    # logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '%(asctime)s;%(module)s;%(funcName)s;%(levelname)s;%(message)s')
+    # logger.propagate = False
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.INFO)
     ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    # logger.addHandler(ch)
+
     fh = logging.FileHandler('portal.log')
     fh.setLevel(logging.INFO)
     fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    # logger.addHandler(fh)
 
+    for h in app.logger.handlers:
+        app.logger.removeHandler(h)
+    app.logger.addHandler(ch)
+    app.logger.addHandler(fh)
+
+    app.logger.info("Initialized logging")
     import portal.views
 
     return app
