5a5caf0311e5073280e12c445712b0df1b07f8ce
Ilija Vukotic <ivukotic@cern.ch>
2022-11-03 10:30:58 -0500

    3 threads

--- boot.sh
+++ boot.sh
@@ -1,2 +1,2 @@
-exec gunicorn -b :5000 --workers=1 --threads=1 --timeout 120 --log-level=info --access-logfile /tmp/gunicorn.log --error-logfile - "portal:create_app()"
+exec gunicorn -b :5000 --workers=1 --threads=3 --timeout 120 --log-level=info --access-logfile /tmp/gunicorn.log --error-logfile - "portal:create_app()"
 # to log requests to stdout  --access-logfile -
