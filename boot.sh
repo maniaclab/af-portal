60845f80c2342583da387b418d5db277feb0fed1
Ilija Vukotic <ivukotic@uchicago.edu>
2024-10-28 12:33:33 -0500

    starting with app.

--- boot.sh
+++ boot.sh
@@ -1,1 +1,1 @@
-gunicorn -b :5000 --workers=1 --threads=3 --timeout 120 --log-level=info --access-logfile /tmp/gunicorn.log --error-logfile - "portal:create_app()"
+gunicorn -b :5000 --workers=1 --threads=3 --timeout 120 --log-level=info --access-logfile /tmp/gunicorn.log --error-logfile - "portal:app"
